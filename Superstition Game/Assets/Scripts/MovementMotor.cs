cd42e63e5a81de9157dc8c2b95f1e7b92d132ae9
unknown <cloutc@SA2411-PC14.win.rpi.edu>
2016-05-13 17:01:53 -0400

    Setup initial player scripts and moved gitignore

---
+++ Superstition Game/Assets/Scripts/MovementMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementMotor : MonoBehaviour {
+
+    CharacterStats stats;
+
+    void Start()
+    {
+        stats = GetComponent<CharacterStats>();
+    }
+
+	public void Move(float horizontal, float vertical)
+    {
+        transform.Translate(horizontal * stats.speed, vertical * stats.speed, 0);
+    }
+}
