ac353b5d780e6e3575cc16a95c777d530540d706
Craig Carlson <cjcarlson20@gmail.com>
2016-05-13 17:07:27 -0400

    pulling gitignore

---
+++ Superstition Game/Assets/Overworld/movePlayerIcon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayerIcon : MonoBehaviour {
+
+	public float speed = 10.0F;
+
+	void Update() {
+		transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+	}
+}
