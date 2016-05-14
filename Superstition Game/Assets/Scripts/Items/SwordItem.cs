eb27235cc8ceec511a6efd55ab435addb8dbd957
DomCristaldi <domeniccristaldi@gmail.com>
2016-05-14 01:35:57 -0400

    Damage
    ++ added damage functions to the Sword Item. Pushing works to get work from others for integration

--- Superstition Game/Assets/Scripts/Items/SwordItem.cs
+++ Superstition Game/Assets/Scripts/Items/SwordItem.cs
@@ -5,5 +5,24 @@ public class SwordItem : ItemBase {
 
     public float damage;
 
+    public bool canDamage = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        //don't proceed unless we can damage
+        if (!canDamage) { return; }
+
+        //only deal damage if the other thing can deal damage
+        if (other.gameObject.GetComponent<CharacterStats>() == null) { return; }
+
+        //WE'RE GOOD TO GO
+
+        CharacterStats otherStats = other.GetComponent<CharacterStats>();
+
+        otherStats.DamageCharacter(damage);
+
+    }
+
+
 	
 }
