456f1d0970086398cf8e30c7c6abed9f47fb2f58
Craig Carlson <cjcarlson20@gmail.com>
2016-05-14 11:34:52 -0400

    distance threshold on ai

--- Superstition Game/Assets/Scripts/Player/AI/AIController.cs
+++ Superstition Game/Assets/Scripts/Player/AI/AIController.cs
@@ -3,64 +3,64 @@ using System.Collections;
 
 public class AIController : MonoBehaviour {
 
-    public GameObject player;
-    Pawn p;
+	private Transform player;
+	Pawn p;
+	public float chase_distance = 10f;
 
 	// Use this for initialization
 	void Start () 
-    {
-        p = GetComponent<Pawn>();
-        player = GameObject.FindGameObjectWithTag("Player");
+	{
+		p = GetComponent<Pawn>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () 
-    {
-        if (!player)
-            return;
+	{
+		if (!player)
+			return;
+		if(Vector3.Distance(player.position,transform.position) < chase_distance) {
+			Vector3 towardPlayer = player.position - transform.position;
 
-        Vector3 towardPlayer = player.transform.position - transform.position;
-
-        if (towardPlayer.magnitude > 5)
-        {
-            Move();
-        }
-        else
-        {
-            int rand = Random.Range(0, 100);
-            if(rand < 1)
-                p.Attack();
-            if(rand > 80)
-            {
-                Move();
-            }
-        }
+			if (towardPlayer.magnitude > 5) {
+				Move();
+			}
+			else {
+				int rand = Random.Range(0, 100);
+				if (rand < 1)
+					p.Attack();
+				if (rand > 80) {
+					Move();
+				}
+			}
+		}
+		
 	}
 
-    void Move()
-    {
-        Vector3 towardPlayer = player.transform.position - transform.position;
+	void Move()
+	{
+		Vector3 towardPlayer = player.position - transform.position;
 
-        float horizontal = 0;
-        float vertical = 0;
+		float horizontal = 0;
+		float vertical = 0;
 
-        if (towardPlayer.x > 1)
-            horizontal = 1;
-        else if (towardPlayer.x < -1)
-            horizontal = -1;
+		if (towardPlayer.x > 1)
+			horizontal = 1;
+		else if (towardPlayer.x < -1)
+			horizontal = -1;
 
-        if (towardPlayer.y > 1)
-            vertical = 1;
-        else if (towardPlayer.y < -1)
-            vertical = -1;
+		if (towardPlayer.y > 1)
+			vertical = 1;
+		else if (towardPlayer.y < -1)
+			vertical = -1;
 
-        horizontal *= Random.Range(0f, 1);
-        vertical *= Random.Range(0f, 1);
+		horizontal *= Random.Range(0f, 1);
+		vertical *= Random.Range(0f, 1);
 
-        //if (Random.Range(0, 100) > 80)
-        //   horizontal *= 0;
-        //if (Random.Range(0, 100) > 80)
-        //    vertical *= 0;
-        p.Move(horizontal, vertical);
-    }
+		//if (Random.Range(0, 100) > 80)
+		//   horizontal *= 0;
+		//if (Random.Range(0, 100) > 80)
+		//    vertical *= 0;
+		p.Move(horizontal, vertical);
+	}
 }
