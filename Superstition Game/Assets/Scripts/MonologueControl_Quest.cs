2ae99a64936a2b99348a77bb7312d08a384ca738
Craig Carlson <cjcarlson20@gmail.com>
2016-05-14 09:36:31 -0400

    quests in the works

--- Superstition Game/Assets/Scripts/MonologueControl_Quest.cs
+++ Superstition Game/Assets/Scripts/MonologueControl_Quest.cs
@@ -2,59 +2,65 @@
 using System.Collections;
 
 public class MonologueControl_Quest : MonologueControl {
-    public QuestBase quest;
-    public string[] completionDialogue;
+	public string questID;
+	private QuestBase quest;
 
-    public enum QuestType { FETCH, ACTION };
-    public QuestType QType;
+	public string[] completionDialogue;
 
-    override public void Interact(Transform t)
-    {
-        Debug.Log("in interact");
-        if (QType == QuestType.ACTION)
-        {
-            if (quest.currentState == QuestBase.State.COMPLETED || quest.currentState == QuestBase.State.TURNED_IN)
-            {
-                chat_dialogue = completionDialogue;
-            }
+	public enum QuestType { FETCH, ACTION };
+	public QuestType QType;
 
-            is_chatting = true;
-            is_idle = false;
-            is_speaking = false;
+	void Start() {
+		quest = QuestMaster.Instance.Quests[questID];
+	}
 
-            quest.CheckConditions("talk");
-        }
-        else if (QType == QuestType.FETCH)
-        {
-            Debug.Log(quest.Inventory.itemsInInventory.Count);
-            if (quest.Inventory.HasItemOfId(quest.QuestItem.id))
-            {
-                Debug.Log("in FETCH");
+	override public void Interact(Transform t)
+	{
+		Debug.Log("in interact");
+		if (QType == QuestType.ACTION)
+		{
+			if (quest.currentState == QuestBase.State.COMPLETED || quest.currentState == QuestBase.State.TURNED_IN)
+			{
+				chat_dialogue = completionDialogue;
+			}
 
-                chat_dialogue = completionDialogue;
+			is_chatting = true;
+			is_idle = false;
+			is_speaking = false;
 
-                is_chatting = true;
-                is_idle = false;
-                is_speaking = false;
+			quest.CheckConditions("talk");
+		}
+		else if (QType == QuestType.FETCH)
+		{
+			Debug.Log(quest.Inventory.itemsInInventory.Count);
+			if (quest.Inventory.HasItemOfId(quest.QuestItem.id))
+			{
+				Debug.Log("in FETCH");
 
-                quest.CheckConditions("givereward");
-            }
-            /*foreach (InventoryController.ItemSlot itemslot in quest.Inventory.itemsInInventory)
-            {
-                print("hello");
-                if (quest.QuestItem.id == itemslot.item.id)
-                {
-                    Debug.Log("in FETCH");
+				chat_dialogue = completionDialogue;
 
-                    chat_dialogue = completionDialogue;
+				is_chatting = true;
+				is_idle = false;
+				is_speaking = false;
 
-                    is_chatting = true;
-                    is_idle = false;
-                    is_speaking = false;
+				quest.CheckConditions("givereward");
+			}
+			/*foreach (InventoryController.ItemSlot itemslot in quest.Inventory.itemsInInventory)
+			{
+				print("hello");
+				if (quest.QuestItem.id == itemslot.item.id)
+				{
+					Debug.Log("in FETCH");
 
-                    quest.CheckConditions("givereward");
-                }
-            }*/
-        }
-    }
+					chat_dialogue = completionDialogue;
+
+					is_chatting = true;
+					is_idle = false;
+					is_speaking = false;
+
+					quest.CheckConditions("givereward");
+				}
+			}*/
+		}
+	}
 }
--- Superstition Game/Assets/Scripts/Quests/QuestBase.cs
+++ Superstition Game/Assets/Scripts/Quests/QuestBase.cs
@@ -3,38 +3,40 @@ using System.Collections;
 
 public class QuestBase : MonoBehaviour {
 
-    public enum State { UKNOWN, STARTED, COMPLETED, TURNED_IN };
-    public State currentState;
+	public string questID;
 
-    public string Description;
+	public enum State { UKNOWN, STARTED, COMPLETED, TURNED_IN };
+	public State currentState;
 
-    public ItemBase QuestItem;
+	public string Description;
+
+	public ItemBase QuestItem;
 
 
-    public InventoryController Inventory;
+	public InventoryController Inventory;
 
-    virtual public void CheckConditions(string str){
-        try
-        {
-            throw new UnityException();
-        }
-        catch (UnityException e){
-            Debug.LogError("CheckConditions is not defined!\n");
-        }
-    }
+	virtual public void CheckConditions(string str){
+		try
+		{
+			throw new UnityException();
+		}
+		catch (UnityException e){
+			Debug.LogError("CheckConditions is not defined!\n");
+		}
+	}
 
-    virtual public void GiveReward(){
-        //change player stat
-        //change boss enemy
-        //give item to player
-        try
-        {
-            throw new UnityException();
-        }
-        catch (UnityException e)
-        {
-            Debug.LogError("GiveReward is not defined!\n");
-        }
-    }
+	virtual public void GiveReward(){
+		//change player stat
+		//change boss enemy
+		//give item to player
+		try
+		{
+			throw new UnityException();
+		}
+		catch (UnityException e)
+		{
+			Debug.LogError("GiveReward is not defined!\n");
+		}
+	}
 
 }
