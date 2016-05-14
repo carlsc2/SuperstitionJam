c6f05ad6f6be3eeaf42b9375bbe1c45393b220f4
Richard Pietrzak <rpietrzak256@gmail.com>
2016-05-14 05:18:24 -0400

    Fetch quest setup

---
+++ Superstition Game/Assets/Scripts/DropOnDeath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropOnDeath : MonoBehaviour
+{
+
+    public GameObject item;
+    public void OnDestroy()
+    {
+        Instantiate(item,transform.position,Quaternion.identity);
+    }
+
+}
