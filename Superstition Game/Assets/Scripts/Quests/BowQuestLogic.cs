dc04b764f3a3fce3647a6f05b50717fe38ed5ab8
Craig Carlson <cjcarlson20@gmail.com>
2016-05-14 11:22:19 -0400

    bow quest

---
+++ Superstition Game/Assets/Scripts/Quests/BowQuestLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowQuestLogic : MonoBehaviour, Interactable {
+
+	public Sprite fixedsprite;
+	public Sprite brokensprite;
+
+	private SpriteRenderer spr;
+
+	public static bool broken = false;
+
+	void Awake() {
+		spr = GetComponent<SpriteRenderer>();
+		spr.sprite = broken ? brokensprite : fixedsprite;
+	}
+
+	virtual public void Interact(Transform t) {
+		QuestMaster.Instance.Quests["BowQuest"].CheckConditions("pull");
+		spr.sprite = brokensprite;
+		broken = true;
+	}
+}
