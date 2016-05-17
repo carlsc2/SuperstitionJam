5bcb29302aec01ae5afeee3a80b8d5ca985b44f0
Craig Carlson <cjcarlson20@gmail.com>
2016-05-17 03:18:36 -0400

    updated  pawn to use new stat enums

--- Superstition Game/Assets/Scripts/Player/PlayerPawn.cs
+++ Superstition Game/Assets/Scripts/Player/PlayerPawn.cs
@@ -4,229 +4,229 @@ using System.Collections.Generic;
 
 public class PlayerPawn : CharacterPawn {
 
-    public enum state { idle, walk, attack, defend };
-    enum direction { left, right }
-    public state currentState = state.idle;
-    direction facing = direction.left;
-    MovementMotor motor;
-    InventoryController inventory;
-    AnimatorHandler anim;
+	public enum state { idle, walk, attack, defend };
+	enum direction { left, right }
+	public state currentState = state.idle;
+	direction facing = direction.left;
+	MovementMotor motor;
+	InventoryController inventory;
+	AnimatorHandler anim;
 
-    //CharacterStats stats;
+	//CharacterStats stats;
 
-    //SpriteRenderer sr;
+	//SpriteRenderer sr;
 
-    public float defendModifier = 3f;
+	public float defendModifier = 3f;
 
-    //public GameObject swordHitbox;
-    //public GameObject shieldHitbox;
+	//public GameObject swordHitbox;
+	//public GameObject shieldHitbox;
 
-    private HashSet<Transform> interactables;
+	private HashSet<Transform> interactables;
 
-    private Vector3 oscale;
+	private Vector3 oscale;
 
-    
-    protected override void Awake() {
-        base.Awake();
+	
+	protected override void Awake() {
+		base.Awake();
 
-        motor = GetComponent<MovementMotor>();
-        inventory = GetComponent<InventoryController>();
-        anim = GetComponent<AnimatorHandler>();
-        //audSource = GetComponent<AudioSource>();
+		motor = GetComponent<MovementMotor>();
+		inventory = GetComponent<InventoryController>();
+		anim = GetComponent<AnimatorHandler>();
+		//audSource = GetComponent<AudioSource>();
 
-        //sr = GetComponent<SpriteRenderer>();
-        //stats = GetComponent<CharacterStats>();
-        interactables = new HashSet<Transform>();
-        oscale = transform.localScale;
+		//sr = GetComponent<SpriteRenderer>();
+		//stats = GetComponent<CharacterStats>();
+		interactables = new HashSet<Transform>();
+		oscale = transform.localScale;
 
-    }
+	}
 
-    protected override void Start()
-    {
-        base.Start();
-        //swordHitbox.SetActive(false);
-        //shieldHitbox.SetActive(false);
+	protected override void Start()
+	{
+		base.Start();
+		//swordHitbox.SetActive(false);
+		//shieldHitbox.SetActive(false);
 
-    }
+	}
 
-    protected override void Update()
-    {
-        base.Update();
+	protected override void Update()
+	{
+		base.Update();
 
-        /*if (currentState == state.idle)
-            sr.color = Color.white;
-        else if (currentState == state.walk)
-            sr.color = Color.green;
-        else if (currentState == state.attack)
-            sr.color = Color.red;
-        else if (currentState == state.defend)
-            sr.color = Color.blue;*/
-    }
+		/*if (currentState == state.idle)
+			sr.color = Color.white;
+		else if (currentState == state.walk)
+			sr.color = Color.green;
+		else if (currentState == state.attack)
+			sr.color = Color.red;
+		else if (currentState == state.defend)
+			sr.color = Color.blue;*/
+	}
 
-    public override void Attack()
-    {
-        if (currentState != state.attack && currentState != state.defend)
-        {
-            currentState = state.attack;
-            //swordHitbox.SetActive(true);
-            //stats.BeginUseItemInHand(CharacterStats.Hand.Main);
-            BeginUseItemInHand(Hand.Main);
+	public override void Attack()
+	{
+		if (currentState != state.attack && currentState != state.defend)
+		{
+			currentState = state.attack;
+			//swordHitbox.SetActive(true);
+			//stats.BeginUseItemInHand(CharacterStats.Hand.Main);
+			BeginUseItemInHand(Hand.Main);
+		}
+	}
 
-            Invoke("EndAttack", stats.attackTime);
-        }
-    }
+	void EndAttack()
+	{
+		currentState = state.idle;
 
-    void EndAttack()
-    {
-        currentState = state.idle;
+		//stats.EndUseItemInHand(CharacterStats.Hand.Main);
+		EndUseItemInHand(Hand.Main);
 
-        //stats.EndUseItemInHand(CharacterStats.Hand.Main);
-        EndUseItemInHand(Hand.Main);
+		//swordHitbox.SetActive(false);
+	}
 
-        //swordHitbox.SetActive(false);
-    }
+	public override void Defend()
+	{
+		if(currentState != state.attack && currentState != state.defend)
+		{
+			currentState = state.defend;
+			//shieldHitbox.SetActive(true);
+			//stats.BeginUseItemInHand(CharacterStats.Hand.Off);
+			BeginUseItemInHand(Hand.Off);
 
-    public override void Defend()
-    {
-        if(currentState != state.attack && currentState != state.defend)
-        {
-            currentState = state.defend;
-            //shieldHitbox.SetActive(true);
-            //stats.BeginUseItemInHand(CharacterStats.Hand.Off);
-            BeginUseItemInHand(Hand.Off);
+			stats.data[CharacterStats.StatType.MaxSpeed] /= defendModifier;
+		}
+	}
 
-            stats.walkSpeed /= defendModifier;
-        }
-    }
+	public override void EndDefend()
+	{
+		if(currentState == state.defend)
+		{
+			currentState = state.idle;
+			//shieldHitbox.SetActive(false);
+			//stats.EndUseItemInHand(CharacterStats.Hand.Off);
+			EndUseItemInHand(Hand.Off);
 
-    public override void EndDefend()
-    {
-        if(currentState == state.defend)
-        {
-            currentState = state.idle;
-            //shieldHitbox.SetActive(false);
-            //stats.EndUseItemInHand(CharacterStats.Hand.Off);
-            EndUseItemInHand(Hand.Off);
+			stats.data[CharacterStats.StatType.MaxSpeed] *= defendModifier;
+		}
+	}
 
-            stats.walkSpeed *= defendModifier;
-        }
-    }
+	public override void Interact()
+	{
+		//interact with nearest thing
+		Transform nearest = null;
+		float min_dist = Mathf.Infinity;
+		Vector3 current_pos = transform.position;
+		foreach (Transform t in interactables) {
+			if (t == null) { continue; }
 
-    public override void Interact()
-    {
-        //interact with nearest thing
-        Transform nearest = null;
-        float min_dist = Mathf.Infinity;
-        Vector3 current_pos = transform.position;
-        foreach (Transform t in interactables) {
-            if (t == null) { continue; }
+			float dist = Vector3.Distance(t.position, current_pos);
+			if (dist < min_dist) {
+				nearest = t;
+				min_dist = dist;
+			}
+		}
+		if(nearest != null) {
+			nearest.GetComponentInChildren<Interactable>().Interact(transform);
 
-            float dist = Vector3.Distance(t.position, current_pos);
-            if (dist < min_dist) {
-                nearest = t;
-                min_dist = dist;
-            }
-        }
-        if(nearest != null) {
-            nearest.GetComponentInChildren<Interactable>().Interact(transform);
+			anim.Interact();
+		}
+	}
 
-            anim.Interact();
-        }
-    }
+	public override void Move(float horizontal, float vertical)
+	{
+		if (currentState != state.attack)
+		{
+			if (currentState == state.defend)
+			{
+				motor.Move(horizontal, vertical);
+			}
+			else
+			{
+				motor.Move(horizontal, vertical);
+				if (horizontal > 0)
+				{
+					facing = direction.right;
+					transform.localScale = new Vector3(oscale.x, oscale.y, oscale.z);
+				}
+				else if (horizontal < 0)
+				{
+					facing = direction.left;
+					transform.localScale = new Vector3(-oscale.x, oscale.y, oscale.z);
+				}
 
-    public override void Move(float horizontal, float vertical)
-    {
-        if (currentState != state.attack)
-        {
-            if (currentState == state.defend)
-            {
-                motor.Move(horizontal, vertical);
-            }
-            else
-            {
-                motor.Move(horizontal, vertical);
-                if (horizontal > 0)
-                {
-                    facing = direction.right;
-                    transform.localScale = new Vector3(oscale.x, oscale.y, oscale.z);
-                }
-                else if (horizontal < 0)
-                {
-                    facing = direction.left;
-                    transform.localScale = new Vector3(-oscale.x, oscale.y, oscale.z);
-                }
+				//ANIMATION
+				anim.SetWalkBlend(motor.trueMoveDirec.magnitude);
 
-                //ANIMATION
-                anim.SetWalkBlend(motor.trueMoveDirec.magnitude);
+				if (currentState != state.walk)
+					currentState = state.walk;
+			}
+		}
+	}
 
-                if (currentState != state.walk)
-                    currentState = state.walk;
-            }
-        }
-    }
+	public override void Idle()
+	{
+		if (currentState != state.attack && currentState != state.defend && currentState != state.idle)
+		{
+			currentState = state.idle;
+		}
+	}
 
-    public override void Idle()
-    {
-        if (currentState != state.attack && currentState != state.defend && currentState != state.idle)
-        {
-            currentState = state.idle;
-        }
-    }
+	public override void Dodge() {
+		StartCoroutine(DodgeTimer(stats.data[CharacterStats.StatType.DodgeTime],
+			                      stats.data[CharacterStats.StatType.DodgeMaxSpeed],
+			                      stats.data[CharacterStats.StatType.DodgeAcceleration]));
+	}
 
-    public override void Dodge() {
-        StartCoroutine(DodgeTimer(stats.dodgeTime, stats.dodgeSpeed, stats.dodgeAccel));
-    }
+	private IEnumerator DodgeTimer(float time, float speed, float accel) {
 
-    private IEnumerator DodgeTimer(float time, float speed, float accel) {
+		motor.isDodging = true;
+		motor.accel = accel;
+		motor.speed = speed;
+		motor.trueMoveDirec = motor.desiredMoveDirec.normalized * speed;
 
-        motor.isDodging = true;
-        motor.accel = accel;
-        motor.speed = speed;
-        motor.trueMoveDirec = motor.desiredMoveDirec.normalized * speed;
-
-        yield return new WaitForSeconds(time);
-
-
-        motor.isDodging = false;
-        motor.accel = stats.accel;
-        motor.speed = stats.walkSpeed;
-
-        yield return null;
-    }
-
-    public override void SelectItemFromInventory(int hotbarNumber) {
-
-        //inventory.EquipHotbarItem(hotbarNumber);
-
-    }
-
-    public override void KillPawn() {
-        base.KillPawn();
-
-        //Trigger death animation on the model
-        anim.TriggerDeath();
-        
+		yield return new WaitForSeconds(time);
 
 
-        //detatch the model from the GameObject that drives logic
-        rig.transform.parent = null;
+		motor.isDodging = false;
+		motor.accel = stats.data[CharacterStats.StatType.Acceleration];
+		motor.speed = stats.data[CharacterStats.StatType.MaxSpeed];
 
-        //Destroy this gameobject
-        Destroy(gameObject);
+		yield return null;
+	}
 
-    }
+	public override void SelectItemFromInventory(int hotbarNumber) {
 
-    void OnTriggerEnter2D(Collider2D col) {
-        //update interaction list
-        Interactable tmp = col.transform.root.GetComponentInChildren<Interactable>();
-        if(tmp != null) {
-            interactables.Add(col.transform.root);
-        }	
-    }
+		//inventory.EquipHotbarItem(hotbarNumber);
 
-    void OnTriggerExit2D(Collider2D col) {
-        //update interaction list
-        interactables.Remove(col.transform.root);
-    }
-    
+	}
+
+	public override void KillPawn() {
+		base.KillPawn();
+
+		//Trigger death animation on the model
+		anim.TriggerDeath();
+		
+
+
+		//detatch the model from the GameObject that drives logic
+		rig.transform.parent = null;
+
+		//Destroy this gameobject
+		Destroy(gameObject);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		//update interaction list
+		Interactable tmp = col.transform.root.GetComponentInChildren<Interactable>();
+		if(tmp != null) {
+			interactables.Add(col.transform.root);
+		}	
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		//update interaction list
+		interactables.Remove(col.transform.root);
+	}
+	
 }
